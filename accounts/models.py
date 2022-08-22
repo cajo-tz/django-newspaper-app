0d30804592f7bcec6ee0f657d114d313dffbdeb8
Courage -Cajo- <zvomuyacourage@gmail.com>
2022-08-22 13:58:02 +0200

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+    
+
+
+
---
+++ articles/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+    )
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
+from django.views.generic.list import ListView
+from .models import Article
+
+class ArticleListView(LoginRequiredMixin, ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleEditView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Article
+    fields = ['title', 'body']
+    template_name = 'article_edit.html'
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = 'article_create.html'
+    fields = ['title', 'body',]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
